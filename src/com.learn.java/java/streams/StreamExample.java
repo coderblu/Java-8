7232dd689ba2bfca0fda75473d8cea0f084ed41a
Akash Sahu <akashsahu786aim@gmail.com>
2024-05-28 18:29:24 +0530

    end of the day

--- src/com.learn.java/java/streams/StreamExample.java
+++ src/com.learn.java/java/streams/StreamExample.java
@@ -1,7 +1,19 @@
 package streams;
 
+import Data.Student;
+import Data.StudentDataBase;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class StreamExample {
     public static void main(String[] args) {
-
+        Predicate<Student> studentPredicate = (student -> student.getGradeLevel()>=3);
+       Map<String, List<String>> studentMap = StudentDataBase.getAllStudents().stream()
+               .filter((student -> student.getGradeLevel())>=3)
+                .collect(Collectors.toMap(Student::getName,Student::getActivities));
+        System.out.println(studentMap);
     }
 }
